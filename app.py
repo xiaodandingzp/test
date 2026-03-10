9f8db439159d64000d814067024d7d0494f9681b
zhangping2 <zhangping2@yy.com>
2026-03-10 11:27:17 +0800

    增加商品

--- app.py
+++ app.py
@@ -2,9 +2,16 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/test')
 def index():
     return jsonify(True)
+
+@app.route('/products')
+def products():
+    return jsonify([
+        {"name": "商品一", "price": 10},
+        {"name": "商品二", "price": 20}
+    ])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
